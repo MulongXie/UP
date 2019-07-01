9d4dc1002901b887264bb4a5ede88b615a32bc8e
Mulong <dsh15325@163.com>
2019-07-01 21:50:56 +1000

    add server

---
+++ router.js
@@ -0,0 +1,18 @@
+
+function router() {
+    var express = require('express');
+    var app = express();
+    app.use(express.static(__dirname));
+    app.use(express.static(__dirname + '/img'));
+
+    app.get('/', function (req, res) {
+        res.sendfile(__dirname + '\\index.html');
+        // res.send('aaa')
+    });
+
+    app.listen(8000, function () {
+        console.log('Server created with port: 8000\n');
+    });
+}
+
+router()
